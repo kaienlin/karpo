5a3d1bef1463d7a05e96640688158766832833d0
Kai-En Lin <kaienlin1999@gmail.com>
2023-11-17 00:02:09 +0800

    feat: update requests API

--- backend/karpo_backend/web/api/requests/schema.py
+++ backend/karpo_backend/web/api/requests/schema.py
@@ -1,25 +1,52 @@
 import datetime
 import uuid
-from typing import List
+from typing import List, Literal, Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, NonNegativeFloat, NonNegativeInt, PositiveInt
 
+from karpo_backend.db.models.users import UserRead  # type: ignore
 from karpo_backend.web.api.utils import LocationDTO
 
 
 class MatchDTO(BaseModel):
+    """A match between a passenger's request and a ride provided by a driver"""
+
     ride_id: uuid.UUID
     get_on_time: datetime.datetime
     get_off_time: datetime.datetime
+    get_on_location: LocationDTO
+    get_off_location: LocationDTO
+    distance_to_get_on_location: NonNegativeFloat
+    distance_to_get_off_location: NonNegativeFloat
+    driver_source: LocationDTO
+    driver_destination: LocationDTO
+    num_available_seat: PositiveInt
+    other_passengers: List[uuid.UUID]
+    driver_info: UserRead
+    cost: NonNegativeInt
+    driver_route: List[LocationDTO]
+    status: Literal["unasked", "pending", "accepted"]
+    join_id: Optional[uuid.UUID] = None
 
 
 class PostRequestsRequestModel(BaseModel):
     time: datetime.datetime
     source: LocationDTO
+    source_description: Optional[str] = None
     destination: LocationDTO
-    num_people: int
+    destination_description: Optional[str] = None
+    num_people: PositiveInt
 
 
 class PostRequestsResponseModel(BaseModel):
     request_id: uuid.UUID
+    matches: List[MatchDTO]
+
+
+class GetRequestIdResponseModel(PostRequestsRequestModel):
+    create_time: datetime.datetime
+    is_active: bool
+
+
+class GetRequestIdMatchesResponseModel(BaseModel):
     matches: List[MatchDTO]
