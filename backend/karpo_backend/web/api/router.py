3af09e0089016d87849b8d2206eed00abd215bf1
Kai-En Lin <kaienlin1999@gmail.com>
2023-11-24 21:52:57 +0800

    chore: remove unused routes

--- backend/karpo_backend/web/api/router.py
+++ backend/karpo_backend/web/api/router.py
@@ -1,22 +1,10 @@
 from fastapi.routing import APIRouter
 
-from karpo_backend.web.api import (
-    docs,
-    dummy,
-    echo,
-    monitoring,
-    redis,
-    requests,
-    rides,
-    users,
-)
+from karpo_backend.web.api import docs, monitoring, requests, rides, users
 
 api_router = APIRouter()
 api_router.include_router(monitoring.router)
 api_router.include_router(users.router)
 api_router.include_router(docs.router)
-api_router.include_router(echo.router, prefix="/echo", tags=["echo"])
-api_router.include_router(dummy.router, prefix="/dummy", tags=["dummy"])
-api_router.include_router(redis.router, prefix="/redis", tags=["redis"])
 api_router.include_router(requests.router, prefix="/requests")
 api_router.include_router(rides.router, prefix="/rides")
