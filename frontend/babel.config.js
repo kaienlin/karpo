adb846e25461c6a1390ab05642f8ef7e50d316f8
廖政華 <45114413+kamelliao@users.noreply.github.com>
2023-11-25 15:05:19 +0800

    chore(frontend): support import alias through module resolver

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -1,7 +1,27 @@
 module.exports = function (api) {
-  api.cache(true);
+  api.cache(true)
   return {
-    presets: ["babel-preset-expo"],
-    plugins: ["module:react-native-dotenv", "react-native-reanimated/plugin"],
-  };
-};
+    presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['.'],
+          alias: {
+            '~/assets': './assets',
+            '~/components': './src/components',
+            '~/navigation': './src/navigation',
+            '~/redux': './src/redux',
+            '~/screens': './src/screens',
+            '~/services': './src/services',
+            '~/types': './src/types',
+            '~/utils': './src/utils'
+          },
+          extensions: ['.js', '.jsx', '.ts', '.tsx']
+        }
+      ],
+      'module:react-native-dotenv',
+      'react-native-reanimated/plugin'
+    ]
+  }
+}
