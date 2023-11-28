f23aa437e5cdf5340e6ee2bd10b1f2f93d1ccb44
廖政華 <45114413+kamelliao@users.noreply.github.com>
2023-11-28 22:42:26 +0800

    add(frontend): DateTimePicker ceil to nearest 5-min interval

--- frontend/src/components/DateTimePicker.tsx
+++ frontend/src/components/DateTimePicker.tsx
@@ -9,8 +9,11 @@ export default function DateTimePicker({
   date: Date
   setDate: (date: Date) => void
 }) {
+  const roundedDate = new Date(Math.ceil(date.getTime() / 300000) * 300000) // 300000 ms = 1000 * 60 * 5 minutes
   const onChange = (event: RNDateTimePickerEvent, selectedDate: Date | undefined) => {
-    setDate(selectedDate)
+    if (selectedDate) {
+      setDate(selectedDate)
+    }
   }
   // TODO: validate expired date (and set to nears valid date)
   // TODO: add support for Android
@@ -18,7 +21,7 @@ export default function DateTimePicker({
   return (
     <View style={{ flex: 1 }}>
       <RNDateTimePicker
-        value={date}
+        value={roundedDate}
         mode={'datetime'}
         onChange={onChange}
         display="spinner"
