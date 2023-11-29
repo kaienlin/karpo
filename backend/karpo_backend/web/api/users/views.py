ed2abc616c33d18ea4c912ba46592c28d0689be1
Kai-En Lin <kaienlin1999@gmail.com>
2023-11-29 14:24:16 +0800

    feat: add API for querying a users's active items

--- backend/karpo_backend/web/api/users/views.py
+++ backend/karpo_backend/web/api/users/views.py
@@ -1,10 +1,11 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status
 
 from karpo_backend.db.models.users import UserCreate  # type: ignore
 from karpo_backend.db.models.users import UserRead  # type: ignore
 from karpo_backend.db.models.users import UserUpdate  # type: ignore
 from karpo_backend.db.models.users import api_users  # type: ignore
 from karpo_backend.db.models.users import auth_cookie  # type: ignore
+from karpo_backend.web.api.users.schema import GetUserActiveItemsResponse
 
 router = APIRouter()
 
@@ -36,3 +37,25 @@ router.include_router(
     prefix="/auth/cookie",
     tags=["auth"],
 )
+
+
+@router.get(
+    "/users/me/active_items",
+    responses={
+        status.HTTP_401_UNAUTHORIZED: {
+            "description": "Missing token or inactive user.",
+        },
+    },
+    tags=["users"],
+)
+async def get_user_me_active_items() -> GetUserActiveItemsResponse:
+    """
+    Get the active passenger/driver states of the current user.
+
+    #### Response:
+    + **driver_state**: non-null if a ride is active currently.
+    + **passenger_state**: non-null if a request is active currently.
+    If `passenger_state.join_id` is non-null, then there is an *accepted*
+    join request, which means `passenger_state.ride_id` is also non-null.
+    """
+    raise NotImplementedError("QQ")
