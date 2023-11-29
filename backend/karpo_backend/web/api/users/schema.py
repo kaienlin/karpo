4c47d40b00b9cf0d0ff115cdf317c050b0aa13a5
Kai-En Lin <kaienlin1999@gmail.com>
2023-11-29 14:24:16 +0800

    feat: add API for querying a users's active items

--- backend/karpo_backend/web/api/users/schema.py
+++ backend/karpo_backend/web/api/users/schema.py
@@ -1,3 +1,4 @@
+import uuid
 from typing import Optional
 
 from pydantic import Base64Str, BaseModel, Field
@@ -7,3 +8,18 @@ class UserInfoForOthersDTO(BaseModel):
     name: str
     rating: Optional[float] = Field(None, ge=0, le=5.0)
     avatar: Base64Str
+
+
+class DriverStateDTO(BaseModel):
+    ride_id: uuid.UUID
+
+
+class PassengerStateDTO(BaseModel):
+    request_id: uuid.UUID
+    join_id: Optional[uuid.UUID] = None
+    ride_id: Optional[uuid.UUID] = None
+
+
+class GetUserActiveItemsResponse(BaseModel):
+    driver_state: Optional[DriverStateDTO]
+    passenger_state: Optional[PassengerStateDTO]
