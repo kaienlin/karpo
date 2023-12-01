2bc70b0e7a165d33b35be50a2fb111742980d36a
orange-36 <afour505@gmail.com>
2023-12-01 23:45:50 +0800

    feat: update RidesDAO & rides API & reformat

--- backend/karpo_backend/db/dao/joins_dao.py
+++ backend/karpo_backend/db/dao/joins_dao.py
@@ -1,6 +1,6 @@
 import datetime
 import uuid
-from typing import Optional, Literal
+from typing import Literal, Optional
 
 from fastapi import Depends
 from sqlalchemy import select
@@ -26,7 +26,7 @@ class JoinsDAO:
             request_id=request_id,
             ride_id=ride_id,
             # fare=,                          # TBA
-            status="pending",   # status -> driver_response?
+            status="pending",  # status -> driver_response?
             # pick_up_location=,              # TBA
             # pick_up_location_description=,  # TBA
             # drop_off_location=,             # TBA
@@ -38,7 +38,7 @@ class JoinsDAO:
         self.session.add(join)
         await self.session.flush()
         return join.id
-        
+
     async def get_joins_model_by_id(
         self,
         join_id: uuid.UUID,
@@ -48,18 +48,18 @@ class JoinsDAO:
         )
 
         return result.one_or_none()
-    
+
     async def put_joins_model_by_id(
-        self,
-        join_id: uuid.UUID,
-        action: Literal["accpet", "reject"]
+        self, join_id: uuid.UUID, action: Literal["accpet", "reject"]
     ) -> Optional[JoinsModel]:
         if action == "accept":
-            row_updated = self.session.query.filter(
-                JoinsModel.join == join_id).update(status="accepted")
+            row_updated = self.session.query.filter(JoinsModel.join == join_id).update(
+                status="accepted"
+            )
         else:
-            row_updated = self.session.query.filter(
-                JoinsModel.join == join_id).update(status="rejected")
+            row_updated = self.session.query.filter(JoinsModel.join == join_id).update(
+                status="rejected"
+            )
         await self.session.commit()
 
         return row_updated
