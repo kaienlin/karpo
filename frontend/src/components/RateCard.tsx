9058e3caa82e158321350001335da44e9ea4e5c6
廖政華 <45114413+kamelliao@users.noreply.github.com>
2023-12-01 23:57:00 +0800

    fix(frontend): jumping star rating card

--- frontend/src/components/RateCard.tsx
+++ frontend/src/components/RateCard.tsx
@@ -1,165 +1,164 @@
-import { View, StyleSheet, TextInput } from 'react-native'
-import React, { useCallback, useState } from 'react';
-import { Rating } from '@kolking/react-native-rating';
-import { 
-  Button, 
-  Card,
-  Text, 
-  Icon, 
-  IconProps, 
-  PopoverPlacements
-} from '@ui-kitten/components'
+import { useState } from 'react'
+import { FlatList, StyleSheet, TextInput, View } from 'react-native'
+import { Rating } from '@kolking/react-native-rating'
+import { Button, Card, Text } from '@ui-kitten/components'
 
-const ChatIcon = (props: IconProps) => <Icon {...props} name="message-circle" />
-const PhoneIcon = (props: IconProps) => <Icon {...props} name="phone" />
+import type { User } from '~/types/data'
 
-interface FooterProps {
-  origin2route: number
-  destination2route: number
-}
+const templateComments = ['超級細心', '非常友善', '交到一個新朋友']
 
 interface CardProps {
-  name: string
+  userInfo: Pick<User, 'id' | 'name'>
+  onBlacklist?: (userId: string) => void
+  onSubmit?: (userId: string, rating: number, comment: string) => void
 }
 
-
-function Footer ({
-  origin2route,
-  destination2route
-}: FooterProps) {
-  return (
-    <View style={{ margin: 10 }}>
-      <Text style={styles.lightText}>起點 / 終點與路線最短距離： {origin2route} km / {destination2route} km</Text>
-    </View>
-  )
-}
-
-export default function RateCard ({
-  name,
+export default function RateCard({
+  userInfo,
+  onBlacklist = () => {},
+  onSubmit = () => {}
 }: CardProps) {
+  const { id, name } = userInfo
+  const [rating, setRating] = useState<number>(0)
+  const [comment, setComment] = useState<string>('')
 
-    const [rating, setRating] = useState<number>(0);
-    const [comment, setComment] = useState<string>("");
+  const handleBlacklist = () => {
+    onBlacklist(id)
+  }
 
-    const handleChange = useCallback(
-        (value: number) => setRating(Math.round((rating + value) * 5 / 10)),
-        [rating],
-    );
-
-    const handleCommentChange = (text: string) => {
-        setComment(text);
-    };
-
-    const handleSubmit = () => {
-        if (comment.trim() !== '') {
-        setComment('');
-        }
-    };
-    return (
+  const handleSubmit = () => {
+    if (comment.trim() !== '') {
+      setComment('')
+    }
+    onSubmit(id, rating, comment)
+  }
+  return (
     <Card
-      style={{ 
-        // marginHorizontal: 20,
-        // marginVertical: 10,
-        // padding: 10, 
+      style={{
         elevation: 5,
-        borderRadius: 12,
+        borderRadius: 12
       }}
+      disabled={true}
     >
       <View style={styles.container}>
         <View style={styles.textContainer}>
           <Text style={styles.text}>與 {name} 共乘體驗如何？</Text>
+          <Button
+            onPress={handleBlacklist}
+            status="basic"
+            size="small"
+            style={{ borderRadius: 100 }}
+          >
+            加入黑名單
+          </Button>
         </View>
 
         <View style={styles.ratingContainer}>
-        <Rating size={40} rating={rating} onChange={handleChange} fillColor={'#f0c414'} touchColor={'#f0c414'} />
+          <Rating
+            size={35}
+            rating={rating}
+            onChange={setRating}
+            fillColor={'#f0c414'}
+            touchColor={'#f0c414'}
+            style={{ marginBottom: -10 }}
+          />
         </View>
+
         <View style={styles.inputContainer}>
-        <TextInput
+          {/* TODO: append button text to comment */}
+          <FlatList
+            data={templateComments}
+            horizontal={true}
+            style={{ paddingVertical: 10 }}
+            ItemSeparatorComponent={() => <View style={{ width: 10 }} />}
+            renderItem={({ item }) => (
+              <Button size="small" appearance="outline" style={{ borderRadius: 100 }}>
+                {item}
+              </Button>
+            )}
+          />
+          <TextInput
             style={styles.input}
-            placeholder="Additional comments..."
+            placeholder="留下您的建議"
             autoCapitalize="none"
             multiline={true}
-            onChangeText={handleCommentChange}
-        />
-        <Button
-            style={styles.button}
-            size="large"
-            onPress={handleSubmit}
-        >
+            onChangeText={setComment}
+          />
+          <Button style={styles.button} size="large" onPress={handleSubmit}>
             提交評價
-        </Button>
+          </Button>
         </View>
       </View>
     </Card>
   )
-};
+}
 
 const styles = StyleSheet.create({
-    
-    container: {
+  container: {
     //   marginVertical: 30,
     //   alignItems: 'flex-start',
-      backgroundColor: '#ffffff',
-      borderRadius: 12,
-      // padding: 10,
-      shadowColor: '#000',
-      shadowOffset: {
-        width: 0,
-        height: 2,
-      },
-      shadowOpacity: 0.25,
-      shadowRadius: 3.84,
-      elevation: 5,
+    backgroundColor: '#ffffff',
+    borderRadius: 12,
+    // padding: 10,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2
     },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5
+  },
 
-    textContainer: {
-      marginLeft: 20,
-      marginBottom: 15,
-    },
-    text: {
-      fontSize: 19,
-      marginTop: 20,
-    },
-    ratingContainer: {
-      alignItems: 'center',
-    },
-    inputContainer: {
-      marginVertical: 15,
-      marginHorizontal: 15,
-      justifyContent: 'center',
-    },
-    input: {
-      borderRadius: 8,
-      backgroundColor: "#eeeef1",
-      height: 140,
+  textContainer: {
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between'
+  },
+  text: {
+    fontSize: 15
+  },
+  ratingContainer: {
+    alignItems: 'center'
+  },
+  inputContainer: {
+    marginVertical: 15,
+    marginHorizontal: 15,
+    justifyContent: 'center'
+  },
+  input: {
+    borderRadius: 8,
+    backgroundColor: '#eeeef1',
+    height: 140,
     //   width: 316,
-      fontSize: 16,
-      textAlign: 'left',
-      textAlignVertical: 'top',
-      fontWeight: 'normal',
-      letterSpacing: 1,
-      lineHeight: 22,
-      padding: 15,
-      paddingTop: 15,
-      alignItems: 'center',
-      gap: 15,
-    },
-    buttonContainer: {
-      marginVertical: 5,
-      paddingHorizontal: 40,
-    },
-    button: {
-      marginTop: 15,
-      borderRadius: 33,
-      backgroundColor: '#f0c414',
-      marginHorizontal: 10,
-    },
-    blacklist: {
-      marginLeft: 220,
-      backgroundColor: 'gray',
-    },
-    lightText: {
-        color: '#5A5A5A'
-      },
-    });
-    
+    fontSize: 16,
+    textAlign: 'left',
+    textAlignVertical: 'top',
+    fontWeight: 'normal',
+    letterSpacing: 1,
+    lineHeight: 22,
+    padding: 15,
+    paddingTop: 15,
+    alignItems: 'center',
+    gap: 15
+  },
+  buttonContainer: {
+    marginVertical: 5,
+    paddingHorizontal: 40
+  },
+  button: {
+    marginTop: 15,
+    borderRadius: 33
+    // backgroundColor: '#f0c414',
+    // marginHorizontal: 10
+  },
+  blacklist: {
+    marginLeft: 220,
+    backgroundColor: 'gray'
+  },
+  lightText: {
+    color: '#5A5A5A'
+  }
+})
--- frontend/src/screens/Rate/Rate.tsx
+++ frontend/src/screens/Rate/Rate.tsx
@@ -1,56 +1,56 @@
-import React, { useCallback, useState } from 'react';
-import { StyleSheet, View, Text, TextInput, FlatList } from 'react-native';
-import { Rating } from '@kolking/react-native-rating';
-import { Button } from "@ui-kitten/components";
-import { SafeAreaView } from "react-native-safe-area-context";
-import RateCard from '../components/RateCard';
+import { FlatList, StyleSheet } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
 import { type NativeStackScreenProps } from '@react-navigation/native-stack'
+import { Icon, TopNavigation, TopNavigationAction, type IconProps } from '@ui-kitten/components'
 
+import RateCard from '~/components/RateCard'
 
 // interface RateScreenProps {
 //   navigation: any; // 根據實際情況更改類型
 // }
 const driverInfoList = [
-    { name: '本丸', },
-    { name: 'Topi', },
-    { name: 'Chako', },
-  ]
+  { id: '000', name: '本丸' },
+  { id: '001', name: 'Topi' },
+  { id: '002', name: 'Chako' }
+]
 
 type RateScreenProps = NativeStackScreenProps<HomeStackParamList, 'RateScreen'>
 
-
-export default function RateScreen ({ navigation }: RateScreenProps) {
-
+export default function RateScreen({ navigation }: RateScreenProps) {
   return (
     <SafeAreaView style={styles.root}>
-      <View style={styles.headerContainer}>
-        <Text style={styles.headerText}>已順利完成行程！</Text>
-      </View>
-
-      <FlatList 
-        data={driverInfoList}
-        renderItem={({item}) => <RateCard {...item} />}
+      <TopNavigation
+        alignment="center"
+        title="已順利完成行程！"
+        accessoryLeft={() => (
+          <TopNavigationAction
+            icon={(props: IconProps) => <Icon {...props} name="arrow-back" />}
+            onPress={() => {
+              navigation.navigate('HomeScreen')
+            }}
+          />
+        )}
       />
 
+      <FlatList data={driverInfoList} renderItem={({ item }) => <RateCard userInfo={item} />} />
     </SafeAreaView>
-  );
-};
+  )
+}
 
 const styles = StyleSheet.create({
-    root: {
-      flex: 1,
+  root: {
+    flex: 1
     //   alignItems: 'center',
     //   justifyContent: 'flex-start',
-    },
-    headerContainer: {
-      paddingHorizontal: 20, // 調整 padding
-      marginBottom: 5,
-      alignSelf: 'flex-start', // 左上角對齊
-    },
-    headerText: {
-      fontSize: 25, 
-      fontWeight: 'bold', 
-      marginTop: 20,
-    },
-});
-    
+  },
+  headerContainer: {
+    paddingHorizontal: 20, // 調整 padding
+    marginBottom: 5,
+    alignSelf: 'flex-start' // 左上角對齊
+  },
+  headerText: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    marginTop: 20
+  }
+})
