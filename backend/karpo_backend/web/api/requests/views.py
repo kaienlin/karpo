07f366191ca721d43075c682f1f67ba62c1e2273
Kai-En Lin <kaienlin1999@gmail.com>
2023-12-02 17:53:28 +0800

    feat: implement API for getting matches of a request

--- backend/karpo_backend/web/api/requests/views.py
+++ backend/karpo_backend/web/api/requests/views.py
@@ -1,18 +1,23 @@
 import uuid
+from typing import List, Tuple
 
 from fastapi import APIRouter, HTTPException
 from fastapi.param_functions import Depends
-from shapely import Point, wkb
+from shapely import LineString, Point, wkb
 
 from karpo_backend.db.dao.requests_dao import RequestsDAO
+from karpo_backend.db.models.rides import RidesModel
 from karpo_backend.db.models.users import User, current_active_user  # type: ignore
+from karpo_backend.matching import Match
 from karpo_backend.web.api.requests.schema import (
     GetRequestIdMatchesResponse,
     GetRequestIdResponse,
+    MatchDTO,
     PostRequestsRequest,
     PostRequestsResponse,
 )
-from karpo_backend.web.api.utils import LocationWithDescDTO
+from karpo_backend.web.api.users.schema import UserInfoForOthersDTO
+from karpo_backend.web.api.utils import LocationWithDescDTO, RouteDTO
 
 router = APIRouter()
 
@@ -98,9 +103,11 @@ async def get_request_id(
     responses={404: {"description": "Not Found"}},
     tags=["passenger"],
 )
-async def get_request_id_matches(
+async def get_request_id_matches(  # noqa: WPS210
     request_id: uuid.UUID,
     limit: int = 10,
+    requests_dao: RequestsDAO = Depends(),
+    user: User = Depends(current_active_user),
 ) -> GetRequestIdMatchesResponse:
     """
     Get matches for the request specified `request_id`.
@@ -112,4 +119,43 @@ async def get_request_id_matches(
     + **proximity**: the lower the better.
     + **join_id**: the join_id of a sent join request, null if `status` is "unasked".
     """
-    raise NotImplementedError("QQ")
+    request = await requests_dao.get_requests_model_by_id(request_id)
+    if request is None:
+        raise HTTPException(status_code=404, detail="Item not found")
+
+    if request.user_id != user.id:
+        raise HTTPException(status_code=403, detail="Permission denied")
+
+    matches: List[Tuple[RidesModel, Match]] = await requests_dao.get_request_matches(
+        request,
+        limit,
+    )
+    resp = GetRequestIdMatchesResponse(matches=[])
+    for ride, match in matches:
+        driver_origin: Point = wkb.loads(bytes(ride.origin.data))
+        driver_destination: Point = wkb.loads(bytes(ride.destination.data))
+        ride_route: LineString = wkb.loads(bytes(ride.route.data))
+        match_dto = MatchDTO(
+            ride_id=ride.id,
+            pick_up_time=match.pick_up_time,
+            drop_off_time=match.drop_off_time,
+            pick_up_location=LocationWithDescDTO.from_point(match.pick_up_location),
+            drop_off_location=LocationWithDescDTO.from_point(match.drop_off_location),
+            pick_up_distance=match.pick_up_distance,
+            drop_off_distance=match.drop_off_distance,
+            driver_origin=LocationWithDescDTO.from_point(driver_origin),
+            driver_destination=LocationWithDescDTO.from_point(driver_destination),
+            num_available_seat=1,
+            other_passengers=[],
+            fare=0,
+            driver_info=UserInfoForOthersDTO(name="test"),
+            driver_route=RouteDTO.from_linestring_and_timestamps(
+                ride_route,
+                ride.route_timestamps,
+            ),
+            proximity=match.estimated_travel_time,
+            status="unasked",
+        )
+        resp.matches.append(match_dto)
+
+    return resp
--- backend/karpo_backend/web/api/utils.py
+++ backend/karpo_backend/web/api/utils.py
@@ -2,17 +2,37 @@ import datetime
 from typing import List, Optional, Tuple
 
 from pydantic import BaseModel
+from shapely import LineString, Point
 
 
 class LocationDTO(BaseModel):
     latitude: float
     longitude: float
 
+    @classmethod
+    def from_point(cls, p: Point) -> "LocationDTO":
+        return cls(longitude=p.x, latitude=p.y)
+
 
 class LocationWithDescDTO(LocationDTO):
     description: Optional[str] = None
+
+    @classmethod
+    def from_point(cls, p: Point) -> "LocationWithDescDTO":
+        return cls(longitude=p.x, latitude=p.y)
 
 
 class RouteDTO(BaseModel):
     route: List[Tuple[float, float]]
     timestamps: List[datetime.datetime]
+
+    @classmethod
+    def from_linestring_and_timestamps(
+        cls,
+        route: LineString,
+        timestamps: List[datetime.datetime],
+    ) -> "RouteDTO":
+        return cls(
+            route=[(p[0], p[1]) for p in route.coords],
+            timestamps=list(timestamps),
+        )
