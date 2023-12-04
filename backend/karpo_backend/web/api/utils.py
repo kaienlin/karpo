7b8e545563733359512468db1a76819f7657840d
Kai-En Lin <kaienlin1999@gmail.com>
2023-12-04 20:34:48 +0800

    feat: add some common functions for working with locations

--- backend/karpo_backend/web/api/utils.py
+++ backend/karpo_backend/web/api/utils.py
@@ -1,8 +1,10 @@
 import datetime
 from typing import List, Optional, Tuple
 
+from geoalchemy2 import WKBElement
 from pydantic import BaseModel
-from shapely import LineString, Point
+from pyproj import Geod
+from shapely import LineString, Point, wkb
 
 
 class LocationDTO(BaseModel):
@@ -18,8 +20,17 @@ class LocationWithDescDTO(LocationDTO):
     description: Optional[str] = None
 
     @classmethod
-    def from_point(cls, p: Point) -> "LocationWithDescDTO":
-        return cls(longitude=p.x, latitude=p.y)
+    def from_wkb(
+        cls,
+        wkb_elem: WKBElement,
+        desc: Optional[str] = None,
+    ) -> "LocationWithDescDTO":
+        p: Point = wkb.loads(bytes(wkb_elem.data))
+        return cls(longitude=p.x, latitude=p.y, description=desc)
+
+    @classmethod
+    def from_point(cls, p: Point, desc: Optional[str] = None) -> "LocationWithDescDTO":
+        return cls(longitude=p.x, latitude=p.y, description=desc)
 
 
 class RouteDTO(BaseModel):
@@ -36,3 +47,21 @@ class RouteDTO(BaseModel):
             route=[(p[0], p[1]) for p in route.coords],
             timestamps=list(timestamps),
         )
+
+    @classmethod
+    def from_wkb_and_timestamps(
+        cls,
+        route_wkb: WKBElement,
+        timestamps: List[datetime.datetime],
+    ) -> "RouteDTO":
+        route: LineString = wkb.loads(bytes(route_wkb.data))
+        return cls(
+            route=[(p[0], p[1]) for p in route.coords],
+            timestamps=list(timestamps),
+        )
+
+
+def get_distance_between_wkb_points(wkb1: WKBElement, wkb2: WKBElement) -> float:
+    p1: Point = wkb.loads(wkb1.data)
+    p2: Point = wkb.loads(wkb2.data)
+    return Geod(ellps="WGS84").geometry_length(LineString([p1, p2]))
