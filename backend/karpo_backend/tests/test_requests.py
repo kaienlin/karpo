a722d4a87c8a11de7c81c19126f534c1f981d702
orange-36 <afour505@gmail.com>
2023-12-10 22:08:19 +0800

    test: update test rides

--- backend/karpo_backend/tests/test_requests.py
+++ backend/karpo_backend/tests/test_requests.py
@@ -157,9 +157,17 @@ async def test_get_matches(
                 100,
             ],
         },
-        "waypoints": [
-            (0.001, 0),
-            (0.001, -0.004),
+        "intermediates": [
+            {
+                "latitude": 0,
+                "longitude": 0.001,
+                "description": "intermediate 1"
+            },
+            {
+                "latitude": -0.004,
+                "longitude": 0.001,
+                "description": "intermediate 2"
+            },
         ],
         "departure_time": "2023-12-08T02:56:00.000Z",
         "num_seats": 1,
