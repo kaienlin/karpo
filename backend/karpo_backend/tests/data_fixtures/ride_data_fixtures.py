c38816af705136d4c8a60585479e33e5ada0caa9
orange-36 <afour505@gmail.com>
2023-12-10 22:08:19 +0800

    feat: update test rides

--- backend/karpo_backend/tests/data_fixtures/ride_data_fixtures.py
+++ backend/karpo_backend/tests/data_fixtures/ride_data_fixtures.py
@@ -1,23 +1,61 @@
 import pytest
 
 test_ride_1 = {
+    "origin": {
+        "latitude": 0.001,
+        "longitude": 0,
+        "description": "A",
+    },
+    "destination": {
+        "latitude": -0.005,
+        "longitude": 0.003,
+        "description": "B",
+    },
+    "route": {
+        "steps": [
+            [(0, 0.001), (0.001, 0)],
+            [(0.001, 0), (0.001, -0.002), (0.001, -0.004)],
+            [(0.001, -0.004), (0.002, -0.004), (0.003, -0.005)],
+        ],
+        "durations": [
+            30,
+            180,
+            100,
+        ],
+    },
+    "intermediates": [
+        {
+            "latitude": 0,
+            "longitude": 0.001,
+            "description": "intermediate 1"
+        },
+        {
+            "latitude": -0.004,
+            "longitude": 0.001,
+            "description": "intermediate 2"
+        },
+    ],
+    "departure_time": "2023-12-08T02:56:00.000Z",
+    "num_seats": 1,
+}
+test_ride_2 = {
     "label": "new ride 1",
     "origin": {
-        "latitude": 0,
+        "latitude": 1,
         "longitude": 0,
         "description": "string"
     },
     "destination": {
-        "latitude": 0,
-        "longitude": 5,
+        "latitude": 5,
+        "longitude": 0,
         "description": "string"
     },
     "route": {
         "steps": [
             [(0, 0), (0, 1)],
-            [(0, 1), (0, 2), (0, 3), (0, 5)]
+            [(0, 1), (0, 2), (0, 3), (0, 4)]
         ],
-        "durations": [1,2]
+        "durations": [1,7]
     },
     "intermediates": [
         {
@@ -29,29 +67,6 @@ test_ride_1 = {
     "departure_time": "2023-12-09T08:00:00.556Z",
     "num_seats": 4
 }
-test_ride_2 = {
-    "label": "new ride 2",
-    "origin": {
-        "latitude": 0,
-        "longitude": 5,
-        "description": "string"
-    },
-    "destination": {
-        "latitude": 0,
-        "longitude": 0,
-        "description": "string"
-    },
-    "route": {
-        "steps": [
-            [(0, 5), (0, 3), (0, 2), (0, 1), (0, 0)]
-        ],
-        "durations": [3]
-    },
-    "intermediates": [],
-    "departure_time": "2023-12-09T17:30:00.556Z",
-    "num_seats": 4
-}
-
 
 
 @pytest.fixture
--- backend/karpo_backend/tests/test_rides.py
+++ backend/karpo_backend/tests/test_rides.py
@@ -2,11 +2,14 @@ from typing import Any, Dict, List
 
 import httpx
 import pytest
+import datetime
 from fastapi import FastAPI, status
 from fastapi.testclient import TestClient
 from httpx import AsyncClient, Cookies
 from pydantic import ValidationError
 from sqlalchemy.ext.asyncio import AsyncSession
+from shapely import Point, LineString, wkb, wkt
+
 
 from karpo_backend.db.dao.rides_dao import RidesDAO
 from karpo_backend.web.api.rides.schema import PostRidesResponse, GetRideSavedRidesResponse
@@ -20,11 +23,11 @@ async def test_creation(
     dbsession: AsyncSession,
 ) -> None:
     """Tests rides instance creation."""
+    req_body = ride_data_1
     url = fastapi_app.url_path_for("post_rides")
-    
     resp = await client_test.post(
         url,
-        json=ride_data_1,
+        json=req_body,
     )
     assert resp.status_code == status.HTTP_200_OK
 
@@ -34,7 +37,50 @@ async def test_creation(
         pytest.fail("invalid response")
     dao = RidesDAO(dbsession)
     resp_db_obj = await dao.get_ride_model_by_id(resp_obj.ride_id)
+
     assert resp_db_obj is not None
+    assert req_body.get("label", None) == None or resp_db_obj.label == req_body["label"]
+    assert resp_db_obj.num_seats == req_body["num_seats"]
+
+    origin_point: Point = wkb.loads(bytes(resp_db_obj.origin.data))
+    assert origin_point.x == req_body["origin"]["longitude"]
+    assert origin_point.y == req_body["origin"]["latitude"]
+    assert resp_db_obj.origin_description == req_body["origin"]["description"]
+
+    destination_point: Point = wkb.loads(bytes(resp_db_obj.destination.data))
+    assert destination_point.x == req_body["destination"]["longitude"]
+    assert destination_point.y == req_body["destination"]["latitude"]
+    assert resp_db_obj.destination_description == req_body["destination"]["description"]
+
+    point_idx = 0
+    db_route: LineString = wkb.loads(bytes(resp_db_obj.route.data))
+    db_route = list(db_route.coords)
+    db_route_timestamps = resp_db_obj.route_timestamps
+    assert len(db_route) == len(resp_db_obj.route_timestamps)
+    for step, duration in zip(req_body["route"]["steps"], req_body["route"]["durations"]):
+        if point_idx == 0: ## check origin
+            assert db_route[point_idx] == step[0]
+            point_idx += 1
+        
+        db_start_time_in_step = db_route_timestamps[point_idx-1]
+        for req_point in step[1:]: ## check other points
+            assert db_route[point_idx] == req_point
+            point_idx += 1
+        db_end_time_in_step = db_route_timestamps[point_idx-1]
+        
+        db_duration_in_step = (db_end_time_in_step - db_start_time_in_step).total_seconds()
+        assert abs(db_duration_in_step - duration) <= 0.00001 ## error of a duration
+
+
+    for intermediate, db_intermediate, db_intermediate_description in zip(req_body["intermediates"], resp_db_obj.intermediates, resp_db_obj.intermediate_descriptions):
+        db_intermediate_point: Point = wkt.loads(db_intermediate)
+        assert db_intermediate_point.x == intermediate["longitude"] 
+        assert db_intermediate_point.y == intermediate["latitude"] 
+        assert db_intermediate_description == intermediate["description"] 
+
+    assert resp_db_obj.departure_time == datetime.datetime.fromisoformat(req_body["departure_time"].replace("Z", "+00:00"))
+
+
     return resp_db_obj.user_id
 
 @pytest.mark.anyio
