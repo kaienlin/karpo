de79040e26b6e96d831c0eb264f34ef4921b89c6
Kai-En Lin <kaienlin1999@gmail.com>
2023-12-17 13:31:37 +0800

    fix: trying to fix database serialization error

--- backend/karpo_backend/web/application.py
+++ backend/karpo_backend/web/application.py
@@ -1,15 +1,47 @@
+import time
 from importlib import metadata
 from pathlib import Path
+from random import uniform
 
-from fastapi import FastAPI
+from asyncpg.exceptions import SerializationError
+from fastapi import FastAPI, Request
 from fastapi.responses import UJSONResponse
 from fastapi.staticfiles import StaticFiles
+from loguru import logger
+from sqlalchemy.exc import DBAPIError
+from starlette.middleware.base import BaseHTTPMiddleware
 
 from karpo_backend.logging import configure_logging
 from karpo_backend.web.api.router import api_router
 from karpo_backend.web.lifetime import register_shutdown_event, register_startup_event
 
 APP_ROOT = Path(__file__).parent.parent
+
+
+class RetryTxnMiddleware:
+    def _retry_exponential_backoff(
+        self, retry_count: int, max_backoff: int = 0
+    ) -> None:
+        sleep_secs = uniform(0, min(max_backoff, 0.1 * (2**retry_count)))
+        time.sleep(sleep_secs)
+
+    async def __call__(self, request: Request, call_next):
+        retry_cnt = 0
+        max_retries = 3
+        while True:
+            try:
+                result = await call_next(request)
+                return result
+            except DBAPIError as e:
+                if retry_cnt >= max_retries:
+                    raise
+                do_retry = False
+                if isinstance(e.orig, SerializationError):
+                    do_retry = True
+                if do_retry:
+                    logger.warning("transaction failed, retrying...")
+                    retry_count += 1
+                    self._retry_exponential_backoff(retry_cnt)
 
 
 def get_app() -> FastAPI:
@@ -43,5 +75,7 @@ def get_app() -> FastAPI:
         StaticFiles(directory=APP_ROOT / "static"),
         name="static",
     )
+
+    app.add_middleware(BaseHTTPMiddleware, dispatch=RetryTxnMiddleware())
 
     return app
